006e84f6a66347ff9cc21039491cc57f801f317c
Rebin <rebinjoachim@gmail.com>
2026-09-11 22:24:48 +0800

    The search function is workin but for some reason I can only search for Naruto

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,44 @@
 'use client'
-
 import { getAnime } from "@/lib/search"
-import {useState, useEffect} from 'react';
+import { useState } from 'react';
 
-export default function HomePage(){
-  const [anime, setAnime] = useState<any>(null);
+export default function HomePage() {
+  const [search, setSearch] = useState('');
+  const [anime, setAnime] = useState<any[]>([]);
 
-  useEffect(() => {
-
-    async function loadData() {
-      const data = await getAnime('5114');
-      setAnime(data);
+  async function loadData() {
+    if(!search.trim()){
+      return;
     }
+    const data = await getAnime(search);
+    setAnime(data.data);
+  }
 
-    loadData();
-
-  }, [])
-
-  return(
+  return (
     <div>
       <h1 className="text-3xl  text-center mt-5">Find Your Anime</h1>
-      {anime && (
-        <img src={anime.data.images.jpg.image_url} 
-             alt={anime.data.title}        
-        />
-      )}
+
+      <input
+        type="text"
+        placeholder="Search Anime: "
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+
+      />
+
+      <button onClick={loadData}>Search</button>
+
+      {anime.map((item) => (
+        <div key={item.mal_id}>
+
+          <img
+            src={item.images.jpg.image_url}
+            alt={item.title}
+          />
+          <h1>{item.title}</h1>
+
+        </div>
+      ))}
     </div>
   )
 }
--- lib/search.ts
+++ lib/search.ts
@@ -1,10 +1,10 @@
-export async function getAnime(anime: string){
+export async function getAnime(search: string){
     const res = await fetch(
-        
-        `https://api.jikan.moe/v4/anime/${anime}/full`
+        `https://api.jikan.moe/v4/anime?q=${encodeURIComponent(search)}&limit=5`
     );
 
     if(!res.ok){
+        console.log(res.status);
         throw new Error("Failed to fetch anime");
     }
 
