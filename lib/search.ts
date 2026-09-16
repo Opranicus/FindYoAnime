03d7d0250a3e76a22d4169e97f01b873fb38f3bf
Rebin <rebinjoachim@gmail.com>
2026-09-16 22:12:58 +0800

    Added some additional anime info to request to the API

--- lib/search.ts
+++ lib/search.ts
@@ -16,6 +16,8 @@ export async function getAnime(search: string) {
                     }
                     genres
                     episodes
+                    status
+                    description
                 }
             }
         }
